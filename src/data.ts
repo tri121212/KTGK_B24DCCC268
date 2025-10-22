4870140d884253a9c4a3c34dda433fa4c9bf337a
Minh_Trí <tri.16102006@gmail.com>
2025-10-22 17:24:42 +0700

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -4,7 +4,7 @@ export const samplePosts: Post[] = [
   {
     id: '1',
     title: 'Hướng dẫn React Hooks cơ bản',
-    author: 'Nguyễn A',
+    author: 'Nguyễn Văn Hải',
     thumbnail: 'https://citgroup.vn/wp-content/uploads/2024/02/tai-sao-lai-su-dung-reactjs.png',
     content: 'React Hooks giúp bạn quản lý state và lifecycle dễ dàng. ' .repeat(30),
     category: 'Công nghệ',
@@ -13,7 +13,7 @@ export const samplePosts: Post[] = [
   {
     id: '2',
     title: 'Kinh nghiệm du lịch Hà Giang 3 ngày 2 đêm',
-    author: 'Trần B',
+    author: 'Đào Xuân Hoàng',
     thumbnail: 'https://cdn.xanhsm.com/2025/02/89f23fc0-du-lich-ha-giang-tu-tuc-1.jpg',
     content: 'Hà Giang là nơi có cảnh sắc núi non hùng vĩ... '.repeat(30),
     category: 'Du lịch',
@@ -22,7 +22,7 @@ export const samplePosts: Post[] = [
   {
     id: '3',
     title: '10 quán ăn ngon ở Sài Gòn',
-    author: 'Lê C',
+    author: 'Nguyễn Tùng Dương',
     thumbnail: 'https://bloganchoi.com/wp-content/uploads/2023/07/quan-com-ngon-sai-gon-1.jpg',
     content: 'Sài Gòn có rất nhiều món ngon: phở, bún, cơm... '.repeat(30),
     category: 'Ẩm thực',
@@ -31,7 +31,7 @@ export const samplePosts: Post[] = [
   {
     id: '4',
     title: 'Làm sao để cân bằng cuộc sống?',
-    author: 'Phạm D',
+    author: 'Phạm Tiến Đạt',
     thumbnail: 'https://blog.skillhub.vn/wp-content/uploads/2022/07/Ky-nang-can-bang-cuoc-song-giup-tan-sinh-vien-lam-quen-moi-truong-moi-2.png',
     content: 'Cân bằng giữa công việc và cuộc sống là điều quan trọng... '.repeat(30),
     category: 'Đời sống',
@@ -40,7 +40,7 @@ export const samplePosts: Post[] = [
   {
     id: '5',
     title: 'Mẹo nhỏ lập trình hiệu quả',
-    author: 'Hoàng E',
+    author: 'Phúc Đoàn',
     thumbnail: 'https://funix.edu.vn/wp-content/uploads/2022/02/m%E1%BA%B9o-h%E1%BB%8Dc-l%E1%BA%ADp-tr%C3%ACnh.png',
     content: 'Tổ chức mã, viết test và đọc tài liệu giúp bạn nhanh hơn... '.repeat(30),
     category: 'Công nghệ',
